a3e34b9ed3c49e5e1a960eb7c4cadb9eff1ff89d
Heejin <khxxjxx@naver.com>
2021-06-24 03:09:42 +0900

    Udemy React기초강좌 & cost_tracker 만들기

---
+++ React/cost_tracker/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = props => {
+  const onSaveExpenseDateHandler = enteredExpenseDate => {
+    const expenseDate = {
+      ...enteredExpenseDate,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseDate);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseDate={onSaveExpenseDateHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
