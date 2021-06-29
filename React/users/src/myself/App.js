a59dbc6fd54d840a4296d17262dfd1db8b25ddca
Heejin <khxxjxx@naver.com>
2021-06-29 20:08:44 +0900

    Udemy React Users_List 만들기

---
+++ React/users/src/myself/App.js
@@ -0,0 +1,23 @@
+// import React, { useState } from 'react';
+// import UserForm from './components/UserForm';
+// import UI from './components/UI';
+// import './App.css';
+
+// function App() {
+//   const [addUser, setAddUser] = useState([]);
+
+//   const onAddUserHandler = user => {
+//     const newUser = [...addUser];
+//     newUser.unshift(user);
+//     setAddUser(newUser);
+//   };
+
+//   return (
+//     <div>
+//       <UserForm onAddUser={onAddUserHandler} />
+//       <UI addUser={addUser} />
+//     </div>
+//   );
+// }
+
+// export default App;
