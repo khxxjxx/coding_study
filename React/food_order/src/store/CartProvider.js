ad06a7cbe8568e69bbca26f888a4a0a9d110fc95
Heejin <khxxjxx@naver.com>
2021-07-08 00:58:50 +0900

    Cart Add버튼 구현

--- React/food_order/src/store/CartProvider.js
+++ React/food_order/src/store/CartProvider.js
@@ -3,14 +3,31 @@ import CartContext from './cart-context';
 
 const defaultCartState = {
   items: [],
-  totlaAmount: 0,
+  totalAmount: 0,
 };
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
@@ -36,7 +53,7 @@ const CartProvider = props => {
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
-    additem: addItemToCartHandler,
+    addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
 
