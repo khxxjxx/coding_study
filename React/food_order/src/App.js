df18977ff5318a56b9939794776076585c6cd726
Heejin <khxxjxx@naver.com>
2021-07-07 23:59:58 +0900

    Cart구현

--- React/food_order/src/App.js
+++ React/food_order/src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Meals from './components/Meals/Meals';
 import Header from './components/Layout/Header';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, SetCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    SetCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    SetCartIsShown(false);
+  };
+
   return (
-    <React.Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
