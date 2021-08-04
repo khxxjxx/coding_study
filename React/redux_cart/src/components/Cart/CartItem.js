a96778ffb0f101f9563db959cf73e3aa53af64e4
Heejin <khxxjxx@naver.com>
2021-08-04 18:48:03 +0900

    review redux-toolkit

--- React/redux_cart/src/components/Cart/CartItem.js
+++ React/redux_cart/src/components/Cart/CartItem.js
@@ -1,7 +1,31 @@
+import { useDispatch } from 'react-redux';
+
+import { cartActions } from '../../store/cart-slice';
 import classes from './CartItem.module.css';
 
-const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+const CartItem = props => {
+  const dispatch = useDispatch();
+
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeHandler = () => {
+    dispatch(
+      cartActions.removeItemFromCart({
+        id,
+        price,
+      })
+    );
+  };
+
+  const addHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        price,
+        title,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +41,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeHandler}>-</button>
+          <button onClick={addHandler}>+</button>
         </div>
       </div>
     </li>
