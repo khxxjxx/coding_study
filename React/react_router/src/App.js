3908bf4cbfb325814051d02e487d9d1d4236089f
Heejin <khxxjxx@naver.com>
2021-08-07 19:00:32 +0900

    using router Switch and exact

--- React/react_router/src/App.js
+++ React/react_router/src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Products from './pages/Products';
 import Welcome from './pages/Welcome';
 import MainHeader from './components/MainHeader';
@@ -11,15 +11,17 @@ function App() {
         <MainHeader />
       </header>
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/product-detail/:productId">
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route exact path="/products">
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
