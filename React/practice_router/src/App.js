6605dbc827bd214e289ae68d4304ab37ee1eee78
Heejin <khxxjxx@naver.com>
2021-08-09 19:07:04 +0900

    Udemy practice router setting

---
+++ React/practice_router/src/App.js
@@ -0,0 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/quotes">
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
+  );
+}
+
+export default App;
