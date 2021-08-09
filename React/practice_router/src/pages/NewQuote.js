80d2cd1c45b73a3458a259e520251240e42db194
Heejin <khxxjxx@naver.com>
2021-08-09 20:41:51 +0900

    Learn Router useHistory & Prompt

--- React/practice_router/src/pages/NewQuote.js
+++ React/practice_router/src/pages/NewQuote.js
@@ -1,9 +1,16 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = quoteData => {
     console.log(quoteData);
+
+    history.push('./quotes');
   };
+
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
