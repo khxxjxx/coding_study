7a87b06dd74305d7724493b19b583c34ad939c44
Heejin <khxxjxx@naver.com>
2021-08-14 00:18:56 +0900

    SignUp Feedback Message 수정

--- React/react_auth/src/components/Auth/AuthForm.js
+++ React/react_auth/src/components/Auth/AuthForm.js
@@ -5,7 +5,9 @@ import classes from './AuthForm.module.css';
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin(prevState => !prevState);
@@ -17,6 +19,7 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
+    setIsLoading(true);
     if (isLogin) {
     } else {
       fetch(
@@ -33,12 +36,16 @@ const AuthForm = () => {
           },
         }
       ).then(res => {
+        setIsLoading(false);
         if (res.ok) {
           console.log('hi');
         } else {
           return res.json().then(data => {
-            // show an error modal
-            console.log(data);
+            let errorMessage = 'Authentication failed!';
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
           });
         }
       });
@@ -63,7 +70,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
