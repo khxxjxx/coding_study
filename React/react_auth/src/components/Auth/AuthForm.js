5fb4424136f6b7f24a2de9abcbcafb8bc2b1286e
Heejin <khxxjxx@naver.com>
2021-08-14 03:29:53 +0900

    Adding Login API

--- React/react_auth/src/components/Auth/AuthForm.js
+++ React/react_auth/src/components/Auth/AuthForm.js
@@ -20,36 +20,46 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     setIsLoading(true);
+    let url;
     if (isLogin) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA-yqiIEBP18uu_9VtChPfLmhMHYwUTEkc';
     } else {
-      fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA-yqiIEBP18uu_9VtChPfLmhMHYwUTEkc',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      ).then(res => {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA-yqiIEBP18uu_9VtChPfLmhMHYwUTEkc';
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => {
         setIsLoading(false);
         if (res.ok) {
-          console.log('hi');
+          return res.json();
         } else {
           return res.json().then(data => {
             let errorMessage = 'Authentication failed!';
             // if (data && data.error && data.error.message) {
             //   errorMessage = data.error.message;
             // }
-            alert(errorMessage);
+
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        alert(err.message);
       });
-    }
   };
 
   return (
