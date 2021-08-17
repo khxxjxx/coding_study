ac1e97536d9d9e4850a7108163b38f9470739835
Heejin <khxxjxx@naver.com>
2021-08-17 20:39:19 +0900

    Extracting Dymanic Parameter Values

--- NextJS/basic/pages/news/[newsId].js
+++ NextJS/basic/pages/news/[newsId].js
@@ -1,6 +1,12 @@
+import { useRouter } from 'next/router';
+
 // our-domain.com/news/something-important
 
 function DetailPage() {
+  const router = useRouter();
+
+  console.log(router.query.newsId);
+
   return <h1>The Detail Page</h1>;
 }
 
