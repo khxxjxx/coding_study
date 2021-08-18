bdf91942274c66ac34b20dc123593f1a8ea19896
Heejin <khxxjxx@naver.com>
2021-08-19 00:13:52 +0900

    Outputting a List of Meetups

--- NextJS/meetup/pages/index.js
+++ NextJS/meetup/pages/index.js
@@ -1,7 +1,27 @@
 // our-domain.com
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/a/ab/BMW_Welt_Night_cropped.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!',
+  },
+];
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
