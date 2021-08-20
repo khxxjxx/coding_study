0fbd20367f317e949df854e0b88f961f114da306
Heejin <khxxjxx@naver.com>
2021-08-20 22:31:58 +0900

    Adding Custom Components & CSS Modules

--- NextJS/meetup/pages/[meetupId].js
+++ NextJS/meetup/pages/[meetupId].js
@@ -1,1 +1,16 @@
 // our-domain.com/:meetupId
+
+import MeetupDetail from '../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
