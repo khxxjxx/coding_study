ef9c1e4bfd987ead82da95b060faa7a6a9bbd66f
Heejin <khxxjxx@naver.com>
2021-08-24 00:55:58 +0900

    Preparing Paths with "getStaticPaths" & Working With Fallback Pages

--- NextJS/meetup/pages/[meetupId].js
+++ NextJS/meetup/pages/[meetupId].js
@@ -13,4 +13,43 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupDate: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
+        id: meetupId,
+        title: 'A First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'The meetup description',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
