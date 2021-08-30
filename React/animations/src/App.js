ead233eb4a137990ffb061515f0d8aaa9ac7bdcf
Heejin <khxxjxx@naver.com>
2021-08-30 17:22:00 +0900

    Wrapping the Transition Component

--- React/animations/src/App.js
+++ React/animations/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Transition from 'react-transition-group/Transition';
 
 import './App.css';
 import Modal from './components/Modal/Modal';
@@ -25,14 +24,7 @@ class App extends Component {
       <div className="App">
         <h1>React Animations</h1>
 
-        <Transition
-          mountOnEnter
-          unmountOnExit
-          in={this.state.modalIsOpen}
-          timeout={300}>
-          {state => <Modal show={state} closed={this.closeModal} />}
-        </Transition>
-
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         {this.state.modalIsOpen && <Backdrop show />}
 
         <button className="Button" onClick={this.showModal}>
