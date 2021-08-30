8b08912e44b929a3d60584ad7c01992d16a0f6b4
Heejin <khxxjxx@naver.com>
2021-08-30 16:16:51 +0900

    Using the Transition Component

--- React/animations/src/App.js
+++ React/animations/src/App.js
@@ -24,38 +24,17 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <button
-          className="Button"
-          onClick={() =>
-            this.setState(prevState => ({ showBlock: !prevState.showBlock }))
-          }>
-          Toggle
-        </button>
-        <br />
+
         <Transition
-          in={this.state.showBlock}
-          timeout={1000}
           mountOnEnter
-          unmountOnExit>
-          {state => (
-            <div
-              style={{
-                backgroundColor: 'red',
-                width: 100,
-                height: 100,
-                margin: 'auto',
-                transition: 'opacity 1s ease-out',
-                opacity: state === 'exiting' ? 0 : 1,
-              }}
-            />
-          )}
+          unmountOnExit
+          in={this.state.modalIsOpen}
+          timeout={300}>
+          {state => <Modal show={state} closed={this.closeModal} />}
         </Transition>
-        {this.state.modalIsOpen && (
-          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        )}
-        {this.state.modalIsOpen ? (
-          <Backdrop show={this.state.modalIsOpen} />
-        ) : null}
+
+        {this.state.modalIsOpen && <Backdrop show />}
+
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
