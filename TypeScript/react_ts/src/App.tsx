1c769e2df92b3cb787c57ddddd1354330fb96831
Heejin <khxxjxx@naver.com>
2021-09-14 20:14:34 +0900

    Working with Components & TypeScript

--- TypeScript/react_ts/src/App.tsx
+++ TypeScript/react_ts/src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Todos />
+    </div>
+  );
 }
 
 export default App;
