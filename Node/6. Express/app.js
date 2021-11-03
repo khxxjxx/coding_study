93710dcb84adfeff3a3b30a0d23916578a6249d1
Heejin <khxxjxx@naver.com>
2021-11-04 00:21:39 +0900

    About the request

--- Node/6. Express/app.js
+++ Node/6. Express/app.js
@@ -1,8 +1,16 @@
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res, next) => {
-  console.log('get');
+app.get('/sky/:id', (req, res, next) => {
+  // console.log(req.path);
+  // console.log(req.headers);
+
+  // localhost:8080/sky/hxxjxx/?keyword=heejin
+  console.log(req.params);
+  console.log(req.params.id); // hxxjxx
+  console.log(req.query);
+  console.log(req.query.keyword); // heejin
+
   res.send('hi');
 });
 
