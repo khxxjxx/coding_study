ed0b127ab762515148a0aaa22342deada30fb0a2
Heejin <khxxjxx@naver.com>
2021-11-12 23:56:37 +0900

    비동기 에러 처리 하는 방법

--- Node/6. Express/app.js
+++ Node/6. Express/app.js
@@ -3,6 +3,8 @@ import fs from 'fs';
 import fsAsync from 'fs/promises';
 // require('express-async-errors'); // CommonJS module
 import {} from 'express-async-errors';
+// 비동기적인 코드도 최종적으로 에러를 포착할수 있게 해주는 라이브러리
+// Express 버전 5부터는 라이브러리를 설치하지 않아도 가능하다
 
 const app = express();
 
